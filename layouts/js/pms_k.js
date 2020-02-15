b9c58bcafa20e2fa999e4a1588aaa7d1e9229ee1
Kajal <kajal@scaletech.xyz>
2020-02-15 19:10:40 +0530

    - set focus on  task list - text -box not append after click enter solve issue

--- layouts/js/pms_k.js
+++ layouts/js/pms_k.js
@@ -1,36 +1,59 @@
 const nEnterBtnKeyCode = 13;
 $(document).ready(function(){
     //on click of add task button
-    $(document).on('click', '#add_task_btn',  function(e){
-        addTaskObject();
+    $(document).on('click', '#add_task_btn', function (e) {
+        addNewTaskObject();
     });
 
 
     $(document).on('keypress', '.to-do-task', function (e) {
         if (e.keyCode === nEnterBtnKeyCode) {
-          //  console.log('enter button pressed');
-            addTaskObject($(this));
+            addNewTaskObject($(this));
         }
     });
 
+    $(document).on('blur', '.new-task', function (e) {
+        if ($(this).text() === '') {
+            $(this).remove();
+        }
+    });
+
+    // $( function() {
+    //     $( "#task_move_id" ).sortable();
+    //     $( "#task_move_id" ).disableSelection();
+    // } );
+    //
+    // $(".to_do_task_list").on('mouseenter', 'li', function () {
+    //     $( ".to-do-task" ).draggable();
+    // });
+
+    // $( ".to-do-task" ).droppable({
+    //     drop: function( event, ui ) {
+    //         $( this )
+    //             .find( "append-draggable-class" )
+    //             .html( "Dropped!" );
+    //     }
+    // });
+    // $( function() {
+    //     $( ".to-do-task" ).draggable();
+    // });
+
 });
 
-function addTaskObject(currentTask){
-    var taskListLi = $('.to_do_task_list li:first').text();
-    console.log(taskListLi);
-    if(taskListLi ===''){
-
+function addNewTaskObject(currentTask) {
+    if ($('.new-task').length
+        && $('.new-task').text() === '') {
+        return;
     }
     currentTask = (arguments.length >= 1) ? arguments[0] : null;
     var newTask = $('<li>')
         .attr("class", "task to-do-task list-group-item new-task")
-        .attr("contenteditable", true);
-     if(currentTask){
-         currentTask.after(newTask);
-     }else{
-         $(".to_do_task_list").prepend(newTask);
-     }
+        .attr("contenteditable", true)
+    if (currentTask) {
+        currentTask.after(newTask);
+    } else {
+        $(".to_do_task_list").prepend(newTask);
+    }
     newTask.focus();
 }
-
 

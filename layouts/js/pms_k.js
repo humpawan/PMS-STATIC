1842d617ef549d256112313c32e55385873e2600
pawanscaletech <hum2250@gmail.com>
2020-02-17 19:30:39 +0530

    -adding modal box in task_list

--- layouts/js/pms_k.js
+++ layouts/js/pms_k.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
         addNewTaskObject();
     });
 
-
     $(document).on('keypress', '.to-do-task', function (e) {
         if (e.keyCode === nEnterBtnKeyCode) {
             addNewTaskObject($(this));
@@ -37,3 +36,52 @@ function addNewTaskObject(currentTask) {
     newTask.focus();
 }
 
+
+$(document).ready(function(){
+    //on click of add task button
+    $(document).on('click', '#add-sub-task-btn', function (e) {
+        addNewSubTask();
+    });
+
+    $(document).on('keypress', '.sub-task', function (e) {
+        if (e.keyCode === nEnterBtnKeyCode) {
+            addNewSubTask($(this));
+        }
+    });
+
+    $(document).on('blur', '.new-sub-task', function (e) {
+        if ($(this).text() === '') {
+            $(this).remove();
+        }
+    });
+
+});
+
+function addNewSubTask(currentSubTask) {
+    if ($('.new-sub-task').length
+        && $('.new-sub-task').text() === '') {
+        return;
+    }
+    currentSubTask = (arguments.length >= 1) ? arguments[0] : null;
+    var newSubTask = $('<li>')
+        .attr("class", "task sub-task list-group-item new-sub-task")
+        .attr("contenteditable", true)
+    if (currentSubTask ) {
+        currentSubTask .after(newSubTask);
+    } else {
+        $(".sub-task-list").prepend(newSubTask);
+    }
+    newSubTask.focus();
+}
+
+
+const textarea = document.querySelector('textarea');
+textarea.addEventListener('keydown', autosize);
+
+function autosize(){
+    const el = this;
+    setTimeout(function(){
+
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    },0);
+}

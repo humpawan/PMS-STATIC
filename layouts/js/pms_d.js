13be8b00fada6e39d7238347942047d9dec70d27
scaletech <dbavishi10@gmail.com>
2020-02-21 17:02:15 +0530

    board

--- layouts/js/pms_d.js
+++ layouts/js/pms_d.js
@@ -105,3 +105,75 @@ function initPopOver() {
     }).click(showPopover).hover(hidePopover);
 }
 
+// Modal js
+
+$(document).ready(function(){
+    //on click of add task button
+    $(document).on('click', '#add-sub-task-btn', function (e) {
+        addNewSubTask();
+    });
+
+    $(document).on('keypress', '.sub-task', function (e) {
+        if (e.keyCode === nEnterBtnKeyCode) {
+            addNewSubTask($(this));
+        }
+    });
+
+    $(document).on('blur', '.new-sub-task', function (e) {
+        if ($(this).text() === '') {
+            $(this).remove();
+        }
+    });
+
+});
+
+function addNewSubTask(currentSubTask) {
+    if ($('.new-sub-task').length
+        && $('.new-sub-task').text() === '') {
+        return;
+    }
+    currentSubTask = (arguments.length >= 1) ? arguments[0] : null;
+    var newSubTask = $('<li>')
+        .attr("class", "task sub-task list-group-item new-sub-task")
+        .attr("contenteditable", true)
+    if (currentSubTask ) {
+        currentSubTask .after(newSubTask);
+    } else {
+        $(".sub-task-list").prepend(newSubTask);
+    }
+    newSubTask.focus();
+}
+
+//text area expand on task name
+const textarea = document.querySelector('.textarea-auto-resize');
+const textarea1 = document.querySelector('.textarea-description-auto-resize');
+textarea.addEventListener('keydown', autosize);
+textarea1.addEventListener('keydown', autosize);
+function autosize(){
+    const el = this;
+    setTimeout(function(){
+
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    },0);
+}
+
+//editor js
+$('.textarea').summernote({
+    toolbar: [
+        ["style", ["style"]],
+        ["font", ["bold", "underline"]],
+        ["fontname", ["fontname"]],
+        ["para", ["ul", "ol", "paragraph"]],
+        ["table", ["table"]],
+        ["view", ["codeview"]]
+    ],
+});
+
+// select2
+$('.select2').select2()
+
+//Initialize Select2 Elements
+$('.select2bs4').select2({
+    theme: 'bootstrap4'
+})
+
